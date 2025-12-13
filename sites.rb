6b97dddbcfac5cd1f3893c24b43e41b13cba4a54
Jason R. Clark <jason@jasonrclark.net>
2025-12-13 10:15:32 -0800

    Refactor to make easier to filter

---
+++ sites.rb
@@ -0,0 +1,584 @@
+SITES = [
+  [
+    "7th-Circle Pyrite",
+    "https://www.7thcirclepyrite.com/submissions",
+    "fantasy, mystical, <2500, ¢"
+  ],
+  [
+    "After the Storm",
+    "https://medium.com/after-the-storm/submission-guide-for-after-the-storm-472ac07eb0e7",
+    "sf, fantasy, anti-capitalist, 1000-5000, $"
+  ],
+  [
+    "Allegory",
+    "https://www.allegoryezine.com/submissions",
+    "sf, fantasy, horror, <5000, Nov 2025, $"
+  ],
+  [
+    "Altered Reality",
+    "https://www.alteredrealitymag.com/",
+    "sf, fantasy, ¢"
+  ],
+  [
+    "Altitude Press",
+    "https://altitude-press.com/submission-guidelines/",
+    "sf, <2000, $"
+  ],
+  [
+    "Another Realm",
+    "https://anotherealm.com/faq/submit.php",
+    "sf, fantasy, horror, <5000, $"
+  ],
+  [
+    "Analog",
+    "https://analogsf.com/contact-us/writers-guidelines/",
+    "sf, $$$"
+  ],
+  [
+    "Andromeda Spaceways",
+    "https://andromedaspaceways.com/submissions-manager/",
+    "sf, fantasy, horror, $"
+  ],
+  [
+    "AntipodeanSF",
+    "https://www.antisf.com.au/submission-guidelines",
+    "flash, sf, fantasy, australian, twists, <1000"
+  ],
+  [
+    "Apex Magazine",
+    "https://www.apexbookcompany.com/a/blog/apex-magazine/post/apex-magazine-submissions-guidelines",
+    "flash 💚?, sf, fantasy, horror"
+  ],
+  [
+    "Arcturus",
+    "https://arcturusmag.com/submit/",
+    "flash, sf, fantasy, <5000, ¢"
+  ],
+  [
+    "Asimov's",
+    "https://asimovs.com/contact-us/writers-guidelines/",
+    "sf, 1000-20k, $$$"
+  ],
+  [
+    "Asimov Press",
+    "https://press.asimov.com/about",
+    "sf, science focus, $$$"
+  ],
+  [
+    "Augur",
+    "https://augursociety.org/submissions/",
+    "sf, fantasy, $$$"
+  ],
+  [
+    "Baffling Magazine",
+    "https://www.bafflingmag.com/submissions",
+    "sf, fantasy, <1200, queer, $$$"
+  ],
+  [
+    "Tales & Feathers",
+    "https://augursociety.org/submissions/",
+    "fantasy, cozy"
+  ],
+  [
+    "Aurealis",
+    "https://aurealis.com.au/submissions/",
+    "sf, australian, $$"
+  ],
+  [
+    "Beneath Ceaseless Skies",
+    "https://www.beneath-ceaseless-skies.com/submissions/",
+    "fantasy, <15000, $$$"
+  ],
+  [
+    "Bewildering Stories",
+    "https://www.bewilderingstories.com/submissions.html",
+    "sf, fantasy, flash, ¢"
+  ],
+  [
+    "BFS Horizons",
+    "https://britishfantasysociety.org/get-in-touch/bfs-horizons/",
+    "fantasy, horror, <5000, $"
+  ],
+  [
+    "The Bizarchives",
+    "https://thebizarchives.com/submissions/",
+    "sf, fantasy, weird, dark, ¢",
+  ],
+  [
+    "Black Cat Weekly",
+    "https://wildside.moksha.io/publication/black-cat-weekly-science-fiction/guidelines",
+    "sf, fantasy, $",
+  ],
+  [
+    "Black Hare Press",
+    "https://blackharepress.com/blogs/open-calls",
+    "sf, fantasy, dark, <2000, $"
+  ],
+  [
+    "Blue Earth Review",
+    "https://blueearthreview.submittable.com/submit",
+    "sf, fantasy, <4000, ¢"
+  ],
+  [
+    "Bottleneck",
+    "https://www.bottleneck-sf.com/submission-guidelines",
+    "sf, fantasy, 3000-8000, ¢"
+  ],
+  [
+    "The Canadian Sepulchral",
+    "https://joewiwcharuk.wixsite.com/the-canadian-sepulch/submissions",
+    "horror, canadian, 1000-5000, ¢"
+  ],
+  [
+    "Carnage House",
+    "https://carnagehouse.com/submission/subs.php",
+    "horror, 500-5500, $"
+  ],
+  [
+    "The Colored Lens",
+    "https://thecoloredlens.com/?page_id=137752",
+    "sf, fantasy, 500-5000, $"
+  ],
+  [
+    "CommuterLit",
+    "https://commuterlit.com/for-writers/submission-guidelines/",
+    "sf, fantasy, 500-4000, ¢"
+  ],
+  [
+    "Clarkesworld",
+    "https://clarkesworldmagazine.com/submissions/",
+    "sf, fantasy, 1000-22000, $$$"
+  ],
+  [
+    "Corner Bar Magazine",
+    "http://www.cornerbarmagazine.com/how.html",
+    "sf, ¢"
+  ],
+  [
+    "Creation Magazine",
+    "https://sites.google.com/creationmag.org/creationmag/submit",
+    "sf, fantasy, horror, <5000, ¢"
+  ],
+  [
+    "Crepuscular Magazine",
+    "https://www.patreon.com/posts/submission-85137677",
+    "sf, fantasy, horror <250, ¢"
+  ],
+  [
+    "Crow & Cross Keys",
+    "https://crowcrosskeys.com/submit/",
+    "sf, fantasy, folk, flash, <5000, ¢"
+  ],
+  [
+    "The Daily Tomorrow",
+    "https://www.thedailytomorrow.com/submissions",
+    "sf, 2100-3500, serial, ¢"
+  ],
+  [
+    "Del Sol Review",
+    "https://delsolreview.webdelsol.com/guidelines.htm",
+    "sf, fantasy, 300-5000, ¢"
+  ],
+  [
+    "Daily Science Fiction",
+    "https://dailysciencefiction.com/",
+    "flash"
+  ],
+  [
+    "Diabolical Plots",
+    "https://www.diabolicalplots.com/guidelines/",
+    "sf, fantasy, $$$"
+  ],
+  [
+    "Ecological Citizen",
+    "https://ecologicalcitizen.net/submitting-fiction.html",
+    "sf, eco, <2000, ¢"
+  ],
+  [
+    "Eldritch Science",
+    "https://tnfff.org/submissions/",
+    "sf, ¢"
+  ],
+  [
+    "Electric Spec",
+    "https://www.electricspec.com/Submissions.html",
+    "sf, fantasy, horror, <7000, $"
+  ],
+  [
+    "Every Day Fiction",
+    "https://everydayfiction.com/submit-story/",
+    "flash"
+  ],
+  [
+    "F(r)iction",
+    "https://frictionlit.org/about/submit/",
+    "flash, weird, $, fee"
+  ],
+  [
+    "Factor Four Magazine",
+    "https://factorfourmag.com/submissionsinfo/",
+    "sf, fantasy, horror, <1000, flash, $$$"
+  ],
+  [
+    "Fantasy & Science Fiction",
+    "https://www.sfsite.com/fsf/glines.htm",
+    "sf, fantasy, <25k, $$$"
+  ],
+  [
+    "Fantasy Magazine",
+    "https://psychopomp.com/fantasy-magazine-guidelines/",
+    "fantasy, flash < 1500, shorts < 5k, $"
+  ],
+  [
+    "Farthest Star Publishing",
+    "https://www.fartheststarpublishing.com/general-8",
+    "sf, fantasy, novelette, 8000-17000, $"
+  ],
+  [
+    "Five on the Fifth",
+    "https://fiveonthefifth.submittable.com/submit",
+    "horror, flash, ¢"
+  ],
+  [
+    "Flash Point Science Fiction",
+    "https://flashpointsf.com/submissions/",
+    "sf, fantasy, <1000, $"
+  ],
+  [
+    "Free Flash Fiction",
+    "https://freeflashfiction.com/submissions/",
+    "sf, fantasy, horror, flash, <500, ¢"
+  ],
+  [
+    "Freedom Fiction Journal",
+    "https://www.freedomfiction.com/submissions/",
+    "sf, fantasy, flash <500, 1000-12000, ¢"
+
+  ],
+  [
+    "Fusion Fragment",
+    "https://www.fusionfragment.com/submissions/",
+    "sf, $$"
+  ],
+  [
+    "The Future Fire",
+    "https://futurefire.net/guidelines/fiction.html",
+    "sf, social, flash <1000, <17000, $"
+  ],
+  [
+    "Galaxy's Edge",
+    "https://www.galaxysedge.com/submissions/",
+    "sf, $$$"
+  ],
+  [
+    "The Genre Society",
+    "https://www.thegenresociety.com/submissions",
+    "sf, fantasy, horror, ¢"
+  ],
+  [
+    "Harvey Duckman",
+    "https://harvey-duckman-is-alive.ghost.io/submissions/",
+    "sf, fantasy, $"
+  ],
+  [
+    "Haven Speculative",
+    "https://www.havenspec.com/submit/",
+    "sf, fantasy, <6000, $$"
+  ],
+  [
+    "Havok",
+    "https://gohavok.com/submission-guidelines/",
+    "sf, fantasy, themed, flash, 300-1000, ¢"
+  ],
+  [
+    "Hawthorn & Ash",
+    "https://ironfaeriepublishing.com/hawthorn-ash/",
+    "fantasy, horror, flash <1000, $"
+  ],
+  [
+    "The Hooklet's Nook",
+    "https://www.thehooletsnook.com/submissions.html",
+    "sf, fantasy, flash, <300, ¢"
+  ],
+  [
+    "Howdy Dude Paperbacks",
+    "https://howdydudepaperbacks.com/submissions/",
+    "sf, 2000-8000, $"
+  ],
+  [
+    "The Horror Zine",
+    "https://www.thehorrorzine.com/Submission%20Guidelines.html",
+    "horror, sf, fantasy, 1900-4000, ¢"
+  ],
+  [
+    "Interzone",
+    "https://interzone.press/submissions/",
+    "sf, <17500, $$"
+  ],
+  [
+    "Incensepunk",
+    "https://www.incensepunk.com/p/submissions",
+    "sf, religous, $$"
+  ],
+  [
+    "Intrepidus Ink",
+    "https://intrepidusink.com/submissions/",
+    "sf, fantasy, flash <1000, <2500, $"
+  ],
+  [
+    "James Gunn's Ad Astra",
+    "https://www.adastrasf.com/submissions/short-fiction-submission-guidelines/",
+    "sf, fantasy, <7500, $"
+  ],
+  [
+    "Kismet",
+    "https://thekismetmagazine.com/submit/",
+    "sf, fantasy, <10000, ¢"
+  ],
+  [
+    "The Lit Nerds",
+    "https://thelitnerds.com/submit/",
+    "sf, fantasy, <3000, ¢"
+  ],
+  [
+    "Lightspeed",
+    "https://adamant.moksha.io/publication/lightspeed",
+    "sf, fantasy, 1500-10k, <5k pref, $$$"
+  ],
+  [
+    "Manawaker Flash Fiction Podcast",
+    "https://www.manawaker.com/ffp-submissions/",
+    "sf, flash <1000, $"
+  ],
+  [
+    "Moonday Mag",
+    "https://moondaymag.com/submissions",
+    "sf, fantasy, <4000, ¢"
+  ],
+  [
+    "Moonpark Review",
+    "https://moonparkreview.com/submission-guidelines/",
+    "speculative, flash, <750, ¢"
+  ],
+  [
+    "Mystic Mind",
+    "https://kirincomicscontact.wixsite.com/my-site/about-1",
+    "sf, fantasy, <5000, $"
+  ],
+  [
+    "Nature: Futures",
+    "https://www.nature.com/nature/for-authors/other-subs",
+    "sf, hard, 850-950, $$$"
+  ],
+  [
+    "Necessary Fiction",
+    "https://necessaryfiction.submittable.com/submit",
+    "sf, fantasy, ¢"
+  ],
+  [
+    "Neon Dystopia",
+    "https://www.neondystopia.com/?p=100042814",
+    "sf, cyberpunk, $"
+  ],
+  [
+    "New Maps",
+    "https://www.new-maps.com/submissions/",
+    "sf, deindustrial, <10000, $"
+  ],
+  [
+    "new sinews",
+    "https://www.newnewsinews.com/about",
+    "sf, ¢"
+  ],
+  [
+    "Not One of Us",
+    "https://legacyliteraryservices.com/not-one-of-us/",
+    "sf, alienation, $"
+  ],
+  [
+    "Nightmare",
+    "https://adamant.moksha.io/publication/nightmare/guidelines",
+    "horror, $$$"
+  ],
+  [
+    "Orion's Belt",
+    "https://www.orions-belt.net/submissions",
+    "sf, <1200, $"
+  ],
+  [
+    "Penumbric",
+    "https://www.penumbric.com/subs.html",
+    "sf, fantasy, horror, edgy"
+  ],
+  [
+    "Phano",
+    "https://www.phano.co/submissions",
+    "sf, <5000, $"
+  ],
+  [
+    "The Piker Press",
+    "https://www.pikerpress.com/",
+    "sf, horror, <10000, ¢"
+  ],
+  [
+    "Possibilia",
+    "https://www.futureaesthetics.foundation/post/how-to-submit-work-to-possibilia-magazine",
+    "sf, 3000-7000, idea first?, ¢"
+  ],
+  [
+    "Propagule",
+    "https://www.propagule.co/#w-tabs-2-data-w-pane-2",
+    "sf, <10000, $"
+  ],
+  [
+    "Pulp Asylum",
+    "https://www.pulpasylum.com/guidelines.html",
+    "sf, fantasy, <3000, $"
+  ],
+  [
+    "Psychopomp Novellas",
+    "https://psychopomp.com/novella-guidelines/",
+    "novellas"
+  ],
+  [
+    "Razzle Dazzle Cafe",
+    "https://razzledazzlecafe.substack.com/p/submission-guidelines",
+    "sf, 1000-2000, ¢"
+  ],
+  [
+    "Radon Journal",
+    "https://www.radonjournal.com/submit",
+    "sf, <5000, $"
+  ],
+  [
+    "Reckoning",
+    "https://reckoning.press/submit/fiction/",
+    "sf, environmental, <15000, $$$"
+  ],
+  [
+    "Renaissance Review",
+    "https://renaissancerev.weebly.com/submit.html",
+    "sf, <5000, ¢"
+  ],
+  [
+    "Roi Fainéant",
+    "https://www.roifaineantpress.com/submissions-1",
+    "sf, ¢"
+  ],
+  [
+    "Seize the Press",
+    "https://www.seizethepress.com/submissions/",
+    "sf, anticapitalist, $"
+  ],
+  [
+    "Shoreline of Infinity",
+    "https://www.shorelineofinfinity.com/submissions/",
+    "sf"
+  ],
+  [
+    "Slipjack Review",
+    "https://skipjackreview.com/submit/",
+    "sf, eclectic, <5000, ¢"
+  ],
+  [
+    "Small Wonders",
+    "https://smallwondersmag.com/submissions/",
+    "flash, <1000, $$$"
+  ],
+  [
+    "StarShipSofa",
+    "http://www.starshipsofa.com/submissions/",
+    "sf, 3000-7000, $"
+  ],
+  [
+    "State of Matter",
+    "https://stateofmatter.in/submissions/",
+    "sf, 1000-15000, ¢"
+  ],
+  [
+    "Story Bottle",
+    "https://www.storybottleco.org/submissions",
+    "sf, flash <1500, <8000, ¢"
+  ],
+  [
+    "Strange Horizons",
+    "http://strangehorizons.com/submit/fiction-submission-guidelines/",
+    "sf, fantasy, <10k 5k pref, $$$"
+  ],
+  [
+    "Tales from the Crosstimbers",
+    "https://thecrosstimbers.online/open-call/",
+    "sf, 1000-2500, $"
+  ],
+  [
+    "Tall Tale TV",
+    "https://talltaletv.com/submission-guidelines/",
+    "sf, fantasy, <6000, $"
+  ],
+  [
+    "Tamarind",
+    "https://tamarindlit.co.uk/submissions/",
+    "sf, 1500-5000, ¢"
+  ],
+  [
+    "The Deadlands",
+    "https://psychopomp.com/the-deadlands-guidelines/",
+    "sf, fantasy, horror, anthology"
+  ],
+  [
+    "Three-Lobed Burning Eye",
+    "https://www.3lobedmag.com/submissions.html",
+    "sf, dark fantasy, horror, $$$"
+  ],
+  [
+    "Timber Ghost",
+    "https://www.timberghostpress.com/submissions.html#/",
+    "horror, flash <1000, $"
+  ],
+  [
+    "Trembling with Fear",
+    "https://horrortree.com/submissions/",
+    "horror"
+  ],
+  [
+    "Uncanny",
+    "https://www.uncannymagazine.com/submissions/",
+    "sf, 750-10000, $$"
+  ],
+  [
+    "Untold Tales",
+    "https://www.podpage.com/untold-tales/p/submissions/",
+    "sf, fantasy, horror, 2000-5500, ¢"
+  ],
+  [
+    "Utopia Science Fiction",
+    "https://www.utopiasciencefiction.com/submit",
+    "sf, <4000, $$$"
+  ],
+  [
+    "Weird Tales",
+    "https://www.weirdtales.com/submissions",
+    "sf"
+  ],
+  [
+    "White Cat Publications",
+    "https://whitecatpublications.com/guidelines/",
+    "sf, <2500, $"
+  ],
+  [
+    "Worlds of If",
+    "https://worldsofifmagazine.com/submissions",
+    "sf"
+  ],
+  [
+    "Worlds of Possibility",
+    "https://worldsofpossibility.moksha.io/closed",
+    "sf, fantasy"
+  ],
+  [
+    "swamp pink",
+    "https://swamp-pink.charleston.edu/submit/",
+    "sf"
+  ],
+]
+
